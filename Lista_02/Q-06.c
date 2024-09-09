12ea9b1c1fc9e76efd9c714a7f75e890deb196ed
Ronaldo Porto <portofilhoronaldo@gmail.com>
2024-09-08 22:56:34 -0300

    feliz

--- Lista_02/Q-06.c
+++ Lista_02/Q-06.c
@@ -3,25 +3,20 @@
 #include "imagens.h"
 
 void geraImgGrey_R(unsigned char img[480][10], unsigned char tipo, int i, int j);
-int maiorR(unsigned char img[480][10], int maior, int i, int j);
 
 int main () {
     unsigned char tipo, img[480][10];
-    int m;
 
     printf("Tipo: ");
     scanf("%d", &tipo);
 
     geraImgGrey_R(img, tipo, 479, 9);
-    m = maiorR (img, -1, 479, 9);
 
     for (int i=0; i<480; i++){
         for (int j=0; j<10; j++){
             printf("%d\n", img[i][j]);
         }
     }
-
-    printf("Maior: %d", m);
 
     return 0;
 }
@@ -36,17 +31,4 @@ void geraImgGrey_R(unsigned char img[480][10], unsigned char tipo, int i, int j)
     }
     img[i][j] = geraGreyPixel(tipo);
     geraImgGrey_R(img, tipo, i, j - 1);
-}
-
-int maiorR(unsigned char img[480][10], int maior, int i, int j){
-    if (j < 0){
-        return maiorR(img, maior, i - 1, 9);
-    }
-    if (i < 0){
-        return maior;
-    }
-    if (img[i][j] > maior){
-        maior = img[i][j];
-    }
-    return maiorR(img, maior, i, j - 1);
 }
