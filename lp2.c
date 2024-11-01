2535dc19f786cc4105ec4a7546f49968b64cd2ac
Ronaldo Porto <portofilhoronaldo@gmail.com>
2024-10-31 23:31:26 -0300

    natha

---
+++ lp2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char* geraPalavra(int tam);
+
+int main (){
+    
+
+
+
+    return 0;
+}
+
+char* geraPalavra(int tam){
+    char vetV[] = {'a', 'e', 'i', 'o', 'u'};
+    char vetC[] = {'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'};
+    
+}
