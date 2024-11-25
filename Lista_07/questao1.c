28e56b237bb45d66a947293b27e51ba0bc5a42c4
Ronaldo Porto <portofilhoronaldo@gmail.com>
2024-11-25 00:55:18 -0300

    lista

--- Lista_07/questao1.c
+++ Lista_07/questao1.c
@@ -26,15 +26,71 @@ struct imagens{
 
 typedef struct imagens tImagens;
 
-int saveImgGray(imgGray img, char[] desc, char[] fileName);
+//---------------------------------------------------------------------------------------------------------------------------------------------
+
+imgGray alocaImagemGray (int nLin, int nCol);
+
+imgRGB alocaImagemRGB (int nLin, int nCol);
+
+int saveImgGray (imgGray img, char desc[], char fileName[]);
 
 int main (){
+    imgGray picgray;
+    imgRGB picRGB;
+    picgray = alocaImagemGray (5, 10);
+    if (picgray.img == NULL || picgray._img == NULL){
+        printf("Erro ao alocar imagem Gray.\n\n");
+        return 1;
+    }
+    picRGB = alocaImagemRGB (5, 10);
+    if (picRGB._img == NULL || picRGB.img == NULL){
+        printf ("Erro ao alocar imagem RGB.\n\n");
+        return 1;
+    }
 
 
 
 
 
-
+    printf ("Fim do programa.");
+    free (picRGB._img);
+    free (picRGB.img);
+    free (picgray._img);
+    free (picgray.img);
     return 0;
+}
+
+imgGray alocaImagemGray (int nLin, int nCol){
+    imgGray picture;
+    picture.nCol = nCol;
+    picture.nLin = nLin;
+    picture._img = NULL;
+    picture.img = NULL;
+    picture._img = (uchar*) calloc (nLin * nCol, sizeof(uchar));
+    picture.img = (uchar**) calloc (nLin, sizeof(uchar*));
+    if (picture._img == NULL || picture.img == NULL){
+        return picture;
+    }
+    for (int i = 0; i < nLin; i++){
+        picture.img[i] = &picture._img[i * nCol];
+    } 
+    return picture;
+}
+
+imgRGB alocaImagemRGB (int nLin, int nCol){
+    imgRGB pic;
+    pic.nLin = nLin;
+    pic.nCol = nCol;
+    pic._img = NULL;
+    pic .img = NULL;
+    pic._img = (tRGB*) calloc (nLin * nCol, sizeof(tRGB));
+    pic.img = (tRGB**) calloc (nLin, sizeof(tRGB*));
+    if (pic._img == NULL || pic.img == NULL){
+        return pic;
+    }
+    for (int i = 0; i < nLin; i++){
+        pic.img[i] = &pic._img[i * nCol];
+    }
+    return pic;
 }
 
